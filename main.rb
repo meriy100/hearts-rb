6bc936b1e9f28c5d5a9855800c9d5e014ea1d92d
meriy100 <ttattataa@gmail.com>
2019-05-26 23:13:05 +0900

    [Add] hello world

--- main.rb
+++ main.rb
@@ -1,27 +1,13 @@
 $memory = Array.new(1024, 0)
 
-PAIR  = -> x { -> y { -> f { f[x][y] } } }
-LEFT  = -> p { p[-> x { -> y { x } } ] }
-RIGHT = -> p { p[-> x { -> y { y } } ] }
-
-ZERO = -> p { -> x { -> { p } } }
-ONE = -> p { -> x { -> { x[p] } } }
-INCREMENT = -> n { -> p { -> x { p[n[p][x]] } } }
-SLIDE     = -> p { PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]] }
-DECREMENT = -> n { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
-T = -> t { -> f { t } }
-F = -> t { -> f { f } }
-IF = -> b { -> { -> t {  -> f { b[t][f] } } } }
-IS_ZERO = -> n { n[-> x { F }][T] }
-
-WHILE = ->(x) { ->(y) { ->(p) { $memory[p] != 0 ? WHILE[x][y][x[p]] : y[p] } } }
-PINC = ->(n) { ->(p) { n[p + 1] } } # @return Pointer
-PDEC = ->(n) { ->(p) { n[p - 1] } } # @return Pointer
-INC = ->(n) { ->(p) { $memory[p] += 1; n[p] } } # @return Pointer
-DEC = ->(n) { ->(p) { $memory[p] -= 1; n[p] } } # @return Pointer
-GET = ->(n) { ->(p) { $memory[p] = STDIN.getc.ord; n[p] } }
-PUT = ->(n) { ->(p) { STDOUT.print $memory[p].chr('UTF-8'); n[p] } }
-FIN = ->(p) { p }
+WHILE = ->x { ->y { ->p { $memory[p] != 0 ? WHILE[x][y][x[p]] : y[p] } } }
+PINC = ->n { ->p { n[p + 1] } } # @return Pointer
+PDEC = ->n { ->p { n[p - 1] } } # @return Pointer
+INC = ->n { ->p { $memory[p] += 1; n[p] } } # @return Pointer
+DEC = ->n { ->p { $memory[p] -= 1; n[p] } } # @return Pointer
+GET = ->n { ->p { $memory[p] = STDIN.getc.ord; n[p] } }
+PUT = ->n { ->p { STDOUT.print $memory[p].chr('UTF-8'); n[p] } }
+FIN = ->p { p }
 
 
 def gem_node(h, hs)
@@ -42,12 +28,15 @@ def gem_node(h, hs)
     GET[gem_node(hs.shift, hs)]
   when '.'
     PUT[gem_node(hs.shift, hs)]
-  when nil
+  when nil, "\n"
     FIN
   end
 end
 
 
-# gem_node(',', '[>+>+<<-]>.>.'.split(''))[0]
+source_code = <<EOS
+++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
+EOS
+
 # Hello World!
-gem_node('+', '+++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.'.split(''))[0]
+gem_node('>', source_code.split(''))[0]
